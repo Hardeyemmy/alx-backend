cbd06fa88469b144a2525149e6887f3678cd1735
Hardeyemmy <iiadyemi307@gmail.com>
2023-01-24 12:19:13 +0100

    1 simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 """
-Simple Pagination
+Hypermedia pagination
 """
-
 import csv
-from typing import List, Tuple
 import math
+from typing import Tuple, List, Dict, Any
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    return a tuple of size two containing a start
+     index and an end index corresponding to the
+     range of indexes to return in a list for
+     those particular pagination parameters
+    :param page:
+    :param page_size:
+    :return:
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
+
 
 class Server:
-    """
-    Server class to paginate a database of popular baby names
+    """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -17,35 +31,29 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
+        """Cached dataset
         """
-        Cached dataset
-        """
-
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[:1]
+            self.__dataset = dataset[1:]
 
         return self.__dataset
 
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
-            """
-            Returning a tuple containing a start and end index.
-            """
-            return ((page - 1) * page_size, page * page_size)
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        Finds the correct indexes to paginate dataset.
+        Find the correct indexes to paginate dataset
+        :param page:
+        :param page_size:
+        :return:
         """
-
         assert type(page) == int
         assert type(page_size) == int
         assert page > 0
         assert page_size > 0
         csv_size = len(self.dataset())
-        start = index_range(page, page_size)
+        start, end = index_range(page, page_size)
         end = min(end, csv_size)
         if start >= csv_size:
             return []
